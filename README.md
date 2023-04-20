cf27295f56d012915ac0b8cf2f3e86363b758ab2
mostafashraf18 <ma2963629@gmail.com>
2023-04-20 13:12:15 +0200

    starting

---
+++ README.md
@@ -0,0 +1,3 @@
+# Designing My own Chip8
+the start point to design Chip8 Emulator in c (step  by step)
+
---
+++ chip8.h
@@ -0,0 +1,29 @@
+/**
+ * @file : chip8.h
+ * @author: Mostafa Ashraf
+ *
+ * all time used things
+ */
+
+#ifndef __CHIP8_H__
+#define __CHIP8_H__
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+
+#define MEMORY_SIZE 4096
+#define G_COL 32
+#define G_ROW 64
+#define GFX_SIZE (G_ROW * G_COL)
+#define STACK_SIZE 16
+#define KEY_SIZE 16
+
+
+
+
+#endif
